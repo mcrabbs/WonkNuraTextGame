d150d10378f8a37795d1c38f2ddae1d8b6f45b2e
Wonkubus <MichaelAndrewCrabbs@gmail.com>
2017-01-24 12:37:25 -0800

    Updated to Unity 5.5
    Also added CounterBoobs

---
+++ Assets/Scripts/CounterBoobs.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterBoobs : MonoBehaviour {
+
+    public GameObject BarfWad;
+    public Text BarfwadText;
+
+	// Use this for initialization
+	void Start () {
+        BarfwadText.text = "Penis";
+	}
+}
